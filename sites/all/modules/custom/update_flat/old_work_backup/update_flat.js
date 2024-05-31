921107e7ba9106122688aedd974088af3a7009d8
pc-pupu <pc.pupai@gmail.com>
2024-05-31 10:55:29 +0530

    first commit

---
+++ sites/all/modules/custom/update_flat/old_work_backup/update_flat.js
@@ -0,0 +1,42 @@
+	
+	/*jQuery(document).ready(function() {
+		jQuery('.update_rhe_selectize').select2();
+	});*/
+
+	function validate_update_flat_form(){
+		//alert('OK'); 
+		//return false;
+		var formElements =
+		{
+			"flat_type":["Flat Type","selectBox"],
+			"rhe_name":["Name of the RHE","selectBox"],
+			"rhe_block":["Name of the Block","selectBox"],
+			"flat_floor":["Floor No.","selectBox"],
+			"flat_no":["Flat No.", "flatNo"],
+			"flat_status":["Flat Status","selectBox"],
+			"flat_category":["Flat Category","selectBox"]
+			
+		};
+		
+		if(!validateForm(formElements)) {
+			return false;
+		} else {
+			return true;
+		}	
+	}
+	
+	function validate_add_flat_block_form(){
+		//alert('OK'); 
+		//return false;
+		var formElements =
+		{
+			"block_name":["Block Name","frontSlashHyphen"]
+			
+		};
+		
+		if(!validateForm(formElements)) {
+			return false;
+		} else {
+			return true;
+		}	
+	}
